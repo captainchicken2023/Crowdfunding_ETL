634f605a9ecf0f1b817b81ab4af70417c0bc46ca
Sari Rojas <sari.stormdance@gmail.com>
2023-09-01 23:05:08 -0500

    making changes

--- Starter_Files/Crowdfunding_db_Schema.sql
+++ Starter_Files/Crowdfunding_db_Schema.sql
@@ -7,35 +7,36 @@ CREATE TABLE Contacts (
 );
 
 CREATE TABLE Category (
-	category_id INT NOT NULL,
+	category_id VARCHAR(12) NOT NULL,
 	category VARCHAR(30) NOT NULL,
-	PRIMARY KEY (category_id, category)
+	PRIMARY KEY (category_id)
 );
 
 CREATE TABLE Subcategory (
-	subcategory_id INT NOT NULL,
-	sub-category VARCHAR(30) NOT NULL,
-	PRIMARY KEY (subcategory_id, sub-category)
+	subcategory_id VARCHAR(5) NOT NULL,
+	subcategory VARCHAR(30) NOT NULL,
+	PRIMARY KEY (subcategory_id)
 );
 
 CREATE TABLE Campaign (
 	cf_id INT NOT NULL,
+	contact_id INT NOT NULL,
 	company_name VARCHAR(50),
 	description VARCHAR(200),
-	goal INT
-	pledged INT
-	outcome CHAR(12)
-	backers_count INT
+	goal INT,
+	pledged INT,
+	outcome CHAR(12),
+	backers_count INT,
 	country CHAR(2),
 	currency CHAR(3),
 	launched_date DATE,
 	end_date DATE,
 	category VARCHAR(30) NOT NULL,
-	sub-category VARCHAR(30) NOT NULL,
+	subcategory VARCHAR(30) NOT NULL,
 	category_id VARCHAR(5) NOT NULL,
 	subcategory_id VARCHAR(12) NOT NULL,
-	PRIMARY KEY (cf_id),
-	FOREIGN KEY (contact_id) REFERENCES,
-	FOREIGN KEY (category_id, category) REFERENCES Category(category_id, category),
-	FOREIGN KEY (subcategory_id, sub-category) REFERENCES Subcategory(subcategory_id, sub-category)
+	FOREIGN KEY (contact_id) REFERENCES Contacts(contact_id),
+	FOREIGN KEY (category_id) REFERENCES Category(category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES Subcategory(subcategory_id)	
 );
+
