a439b46e4ff9c57318e229cf6f8296d8260cda06
Sari Rojas <sari.stormdance@gmail.com>
2023-09-02 16:12:50 -0500

    Change order and keys one more time

--- Starter_Files/Crowdfunding_db_Schema.sql
+++ Starter_Files/Crowdfunding_db_Schema.sql
@@ -1,18 +1,30 @@
 --Creating Tables with Primary Keys first
-CREATE TABLE Category (
-	category_id VARCHAR(5) NOT NULL,
-	category VARCHAR(30) NOT NULL UNIQUE,
-	PRIMARY KEY (category_id)
-);
 
---https://stackoverflow.com/questions/3168644/can-a-table-field-contain-a-hyphen
 --Because the column "sub-category" contains a hyphen, quotes must be used every time this column is referenced. 
+--Resource https://stackoverflow.com/questions/3168644/can-a-table-field-contain-a-hyphen
 
 CREATE TABLE Subcategory (
 	subcategory_id VARCHAR(12) NOT NULL,
 	"sub-category" VARCHAR(30) NOT NULL UNIQUE,
 	PRIMARY KEY (subcategory_id)
 );
+
+CREATE TABLE Contacts (
+	contact_id INT NOT NULL,
+	first_name CHAR(30) NOT NULL,
+	last_name CHAR(30) NOT NULL,
+	email VARCHAR(50)  NOT NULL,
+	PRIMARY KEY (contact_id)
+);
+
+CREATE TABLE Category (
+	category_id VARCHAR(5) NOT NULL,
+	category VARCHAR(30) NOT NULL UNIQUE,
+	PRIMARY KEY (category_id)
+);
+
+--Creating Table with Foreign Keys
+--Resource https://www.tektutorialshub.com/sql-server/float-real-data-types-in-sql-server/
 
 CREATE TABLE Campaign (
 	cf_id INT NOT NULL,
@@ -31,19 +43,13 @@ CREATE TABLE Campaign (
 	"sub-category" VARCHAR(30) NOT NULL,
 	category_id VARCHAR(5) NOT NULL,
 	subcategory_id VARCHAR(12) NOT NULL,
-	PRIMARY KEY (contact_id),
+	FOREIGN KEY (contact_id) REFERENCES Contacts(contact_id),
 	FOREIGN KEY (subcategory_id) REFERENCES Subcategory(subcategory_id),
 	FOREIGN KEY (category_id) REFERENCES Category(category_id),
 	FOREIGN KEY ("sub-category") REFERENCES Subcategory("sub-category"),
 	FOREIGN KEY (category) REFERENCES Category(category)
 );
 
-CREATE TABLE Contacts (
-	contact_id INT NOT NULL,
-	first_name CHAR(30) NOT NULL,
-	last_name CHAR(30) NOT NULL,
-	email VARCHAR(50)  NOT NULL,
-	FOREIGN KEY (contact_id) REFERENCES Campaign(contact_id)
-);
+
 
 
